76d536bb2a2dd419ec7f98e9ad8636139a57d0df
Kewicoolx <kewicoolx@gmail.com>
2020-03-16 20:21:20 +0200

    accept this

---
+++ CS401 Game/src/engine/Dummy.java
@@ -0,0 +1,14 @@
+package engine;
+
+public class Dummy {
+
+	public Dummy() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("HI");
+	}
+
+}
